00a3eca31abca85fd5bd82fb724cf6ad52ca527a
webdevpalawan <webdevpalawan@gmail.com>
2021-08-19 13:45:57 +0800

    add dynamic settings.py

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -11,21 +11,23 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+DJANGO_MODE = os.getenv('DJANGO_MODE', 'local').lower()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pgwfu06xv5)d$dnxd(ey))u=@a#3i3q_5epeh&m&adms!k!woq'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if DJANGO_MODE == 'local':
+    DEBUG = True
+else:
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
@@ -39,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+if DJANGO_MODE == 'local':
+    INSTALLED_APPS += [
+        'debug_toolbar',
+    ]
+# import pdb;pdb.set_trace()
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -48,6 +55,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if DJANGO_MODE == 'local':
+    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 
 ROOT_URLCONF = 'projectsite.urls'
 
@@ -69,16 +79,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'projectsite.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DJANGO_MODE == 'local':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        } 
     }
-}
+elif DJANGO_MODE == 'production':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+            'PORT': os.getenv('DB_PORT', '3306'),
+        }
+    }    
 
 
 # Password validation
@@ -118,8 +136,30 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'projectsite', 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ADMINS = (
+    ('Jarkko Marasigan', 'webdevpalawan@gmail.com'),
+)
+
+if DJANGO_MODE == 'local':
+    EMAIL_HOST = 'smtp.mailtrap.io',
+    EMAIL_HOST_USER = os.getenv('MAILTRAP_USERNAME')
+    EMAIL_HOST_PASSWORD = os.getenv('MAILTRAP_PASSWORD')
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+elif DJANGO_MODE == 'production':    
+    EMAIL_HOST = 'smtp.sendgrid.net',
+    EMAIL_HOST_USER = os.getenv('SENDGRID_USERNAME')
+    EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_PASSWORD')
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
